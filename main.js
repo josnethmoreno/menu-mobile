5dce1d8636115817390c0a9a1f214cdecebca93c
josnethmoreno <josnethmoreno@gmail.com>
2023-02-07 17:59:25 -0400

    show menu

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 import './style.css'
 import './reset.css'
+
+
+const menu = document.querySelector('.menu')
+const menuToggle = document.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', () => {
+  const show = (menu.getAttribute('data-show') === 'true')
+  menu.setAttribute('data-show', !(show))
+})
