52774be106927cb722329947979bf1c8b7582459
josnethmoreno <josnethmoreno@gmail.com>
2023-02-08 22:10:26 -0400

    transition menu-bg

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ import './reset.css'
 const menu = document.querySelector('.menu')
 const menuToggle = document.querySelector('.menu-toggle');
 
-menuToggle.addEventListener('click', () => {
+const toggleMenu = () => {
   const show = (menu.getAttribute('data-show') === 'true')
   menu.setAttribute('data-show', !(show))
-})
+}
+
+menuToggle.addEventListener('click', toggleMenu)
